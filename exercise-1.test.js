456c92dfa12703eef2e33f2cf6a7f87c1502bb5f
cuonghoang131993 <cuong.hoang@pycogroup.com>
2019-05-13 13:56:02 +0700

    update test description

--- exercise-1.test.js
+++ exercise-1.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const { store, load } = require('./exercise-1');
 
 describe('Implement a store function and a load function', () => {
-    it('should render text when array given', () => {
+    it('should render text with a string-based format \"key1=value1;key2=value2\nkeyA=valueA\" when array given', () => {
         const array = [
             {
                 key1: "value1",
@@ -15,6 +15,8 @@ describe('Implement a store function and a load function', () => {
 
         const text = store(array);
 
+        expect(array[1]['keyA']).toBe('valueA');
+
         expect(text).toEqual(expected);
     })
 
@@ -24,7 +26,7 @@ describe('Implement a store function and a load function', () => {
         expect(() => store(string) ).toThrow();
     })
 
-    it('return array when text given right array type', () => {
+    it('return array when text given with a string-based format \"key1=value1;key2=value2\nkeyA=valueA\"', () => {
         const expected = [
             {
                 key1: "value1",
@@ -37,6 +39,8 @@ describe('Implement a store function and a load function', () => {
 
         const array = load(text);
 
+        expect(array[0]['key1']).toBe('value1');
+        expect(array[0]['key2']).toBe('value2');
         expect(array).toEqual(expected);
     })
 
